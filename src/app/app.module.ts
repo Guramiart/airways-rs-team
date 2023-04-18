1edca6ca9851029d6a9893e7aeaef5bc2774e922
Artem Pasko <guramibox777@gmail.com>
2023-04-18 15:59:21 +0300

    feat: add basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { AirwaysModule } from './airways/airways.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    CoreModule,
+    AirwaysModule,
     BrowserModule,
     AppRoutingModule,
   ],
