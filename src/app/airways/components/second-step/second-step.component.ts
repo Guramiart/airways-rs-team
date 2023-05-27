c9092dc0e9de39049968dc4878c2123f458df550
Artem Pasko <guramibox777@gmail.com>
2023-05-27 23:38:03 +0300

    feat: add step-1 back button logic

--- src/app/airways/components/second-step/second-step.component.ts
+++ src/app/airways/components/second-step/second-step.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { PassengersType } from 'src/app/shared/enums/passengers-type';
 import { Passengers } from '../../models/passengers';
 import { IPassengerInfo } from '../../models/passengerInfo.model';
 import * as PassengerSelect from '../../../redux/selectors/passenger.selector';
@@ -14,6 +15,8 @@ import { StepperService } from '../../../core/services/stepper-service.service';
   styleUrls: ['./second-step.component.scss'],
 })
 export class SecondStepComponent implements OnInit, OnDestroy {
+
+  public type = PassengersType;
 
   public passengers: Passengers;
 
@@ -81,13 +84,13 @@ export class SecondStepComponent implements OnInit, OnDestroy {
 
   public getPassengerInfo(data:{ type:string, info:IPassengerInfo }):void {
     switch (data.type) {
-      case 'adult':
+      case PassengersType.ADULT:
         this.adultInfo.push(data.info);
         break;
-      case 'child':
+      case PassengersType.CHILD:
         this.childInfo.push(data.info);
         break;
-      case 'infant':
+      case PassengersType.INFANT:
         this.infantInfo.push(data.info);
         break;
       default: break;
